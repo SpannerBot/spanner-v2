a90ce8877f7813aca3259048d6e603f2e0047d59
eek7574 <eek@clicksminuteper.net>
2022-03-11 12:15:51 +0000

    Update debug.py

--- src/cogs/official/debug.py
+++ src/cogs/official/debug.py
@@ -110,7 +110,7 @@ class Debug(commands.Cog):
         await ctx.defer(ephemeral=ephemeral)
         raise FileNotFoundError("Artificial error.")
 
-    @commands.slash_command(name="get-error")
+    @commands.slash_command(name="get-error-case", default_permission=True)
     # @permissions.is_owner()
     async def get_error_case(self, ctx: discord.ApplicationContext, case_id: int):
         """Fetches an error case"""
