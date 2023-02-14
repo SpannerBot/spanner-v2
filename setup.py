7c7eef9f03b71b111021b0e0a04224150e29d8dc
eek7574 <53234499+EEKIM10@users.noreply.github.com>
2023-02-14 00:46:09 +0000

    Update setup.py
    Actually adhere to PEP440

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 
 from setuptools import setup, find_packages
 
-spanner_version = subprocess.run(("git", "rev-parse", "--short", "HEAD"), capture_output=True, encoding="utf-8")
+spanner_version = subprocess.run(("git", "rev-list", "--count", "HEAD")), capture_output=True, encoding="utf-8")
 spanner_version = spanner_version.stdout.strip()
 
 with open("requirements.txt") as requirements_txt:
@@ -11,7 +11,7 @@ with open("requirements.txt") as requirements_txt:
 
 setup(
     name="spanner",
-    version="2.1.0p1+" + spanner_version,
+    version="2.1.0p1.dev" + spanner_version,
     py_modules=["spanner"],
     packages=find_packages(),
     install_requires=requirements,
